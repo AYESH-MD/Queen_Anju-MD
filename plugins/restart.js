163615081b298feb1db266a51838f3c104541f09
AYESH-MD <ayeshuditha10383@gmail.com>
2024-08-25 06:55:50 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -13,7 +13,7 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
     if(!isOwner)return
 const {exec} = require("child_process")
-reply("restarting QUEEN ANJU MD...")
+reply("restarting *ᴄʏʙᴇʀ-x-ʙᴏᴛ*")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
